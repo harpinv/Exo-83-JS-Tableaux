ea3cec1d162066d0f17e33a7e2a2b42da170c3ce
vince <vincent.harpin@orange.fr>
2022-12-02 14:00:21 +0100

    exercice 83

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 let prenoms = ["pierre","paul","jacques"];
-    = "<Ton Prénom>";
+prenoms[0] = "vincent";
 
-document.getElementById('viewport'). = ;
+document.getElementById('viewport').innerText = prenoms[0];
 
 
 
